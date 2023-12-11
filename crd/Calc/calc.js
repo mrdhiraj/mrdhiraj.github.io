cbbd9e64f5eb4290e8213f68cab0216117677637
dj <mrcouthy@gmail.com>
2023-12-11 15:29:14 +0545

    add all

--- crd/Calc/calc.js
+++ crd/Calc/calc.js
@@ -2,16 +2,29 @@ var app = new Vue({
     el: '#app',
     data :{
         noofplrs :2,
-        url: "openeye.png",
+        seenurl: "openeye.png",
+        winnerurl: "crown.png",
         seen:false,
-        mal:0
+        mal:0,
+        
     },
     methods :{
         noplrs(){
             app.noofplrs ===6 ?(app.noofplrs=2) :  (app.noofplrs=app.noofplrs+1)        
         },
         seenclicked(){           
-            app.url=app.url== "openeye.png"?"closedeye.png" :  "openeye.png"    
+            app.seenurl=app.seenurl== "openeye.png"?"closedeye.png" :  "openeye.png"    
+        },
+        winnertoggle()
+        {
+            app.winnerurl=app.winnerurl== "crown.png"?"nowin.png" :  "crown.png"    
+        }
+    },
+    computed :{
+        point()
+        {
+            return  this.mal+4 
         }
     }
+
 })
