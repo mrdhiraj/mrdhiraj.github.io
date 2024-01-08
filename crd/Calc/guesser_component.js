4793693da6ac8e81ee25860d406869e051e4fc1a
dj <mrcouthy@gmail.com>
2024-01-08 10:33:53 +0545

    adding samples

--- crd/Calc/guesser_component.js
+++ crd/Calc/guesser_component.js
@@ -1,15 +1,21 @@
 export default {
   data() {
     return {
-      plr:{
-        name:"hi"
+      plr: {
+        name: "hi"
       }
     };
+  }, 
+  methods: {
+    changeFontSize() {
+      this.$refs.myParagraph.style.fontSize = '20px';
+    }
   },
   template: ` <div>
-  <span > {{datum.color}} {{datum.value}} {{datum.probability_total}} </span>
+  <button @click ="changeFontSize">hello</button>
+  <span ref="myParagraph" style="font-size: ${1 + 9}px"> {{datum.color}} {{datum.value}} {{datum.probability_total}} </span>
   </div>`,
-  props :['datum'],
+  props: ['datum'],
   name: "Acard"
 }
 
