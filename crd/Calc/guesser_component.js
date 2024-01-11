bbef9d713c2ff2dbf58ca4dfc3d7428f489b1791
dj <mrcouthy@gmail.com>
2024-01-11 16:13:04 +0545

    update new design

--- crd/Calc/guesser_component.js
+++ crd/Calc/guesser_component.js
@@ -12,9 +12,18 @@ export default {
       this.$emit('my-event', this.datum.color,this.datum.value,"s")
     }
   },
-  template: `<div><span @click="add()" class="btnplus"> + </span>          
-  <span ref="myParagraph" class="score"> {{datum.probability_total}} </span>
-  <span @click="subs()" class="btnminus"> - </span></div>`,
+  template: `<div>
+  <div class="grid-container">
+  <div class="grid-item"><span @click="add()" class="btnplus"> + </span> </div>
+  <div class="grid-item"><span @click="subs()" class="btnminus"> - </span></div>
+  <div class="grid-item"><span @click="add()" >3</span></div>
+  <div class="grid-item"><span @click="subs()" >4</span></div>
+  <div class="grid-item"><span @click="add()" >5</span></div>
+  <div class="grid-item"><span @click="subs()">6</span></div>
+  <div class="grid-item">7</div>
+  <div class="grid-item"> <span ref="myParagraph" class="score"> {{datum.probability_total}} </span></div>
+</div>
+  `,
   props: ['datum'],
   name: "Acard"
 }
